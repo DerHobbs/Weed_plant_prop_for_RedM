86d646524f8f57e53c257c5d24bd3e7d8840d271
Hobbs <101003021+DerHobbs@users.noreply.github.com>
2022-06-11 05:09:29 +0200

    Add files via upload

---
+++ prop_weed/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "rdr3"
+author 'Hobbs@RisingSunRP'
+
+this_is_a_map "yes"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+files {
+	'stream/prop_weed_ytyp.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_weed_ytyp.ytyp'
